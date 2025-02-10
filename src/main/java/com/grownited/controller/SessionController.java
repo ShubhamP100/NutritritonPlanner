67bdc773c0f9084b6542806abcf4f877db2bf9e5
Shubham <sp577813@gmail.com>
2025-02-10 14:24:15 +0530

    done

---
+++ src/main/java/com/grownited/controller/SessionController.java
@@ -0,0 +1,10 @@
+package com.grownited.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class SessionController {
+    @GetMapping ("signup") 
+	public String signup() {
+		  return "Signup";
+	}
+}
